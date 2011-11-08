a5e88eebb0aa1160287439cc7e9f64dd1cc4220f
Jan Minář <rdancer@rdancer.org>
2011-11-08 11:57:33 +0000

    Count of messages received from individual client IDs are tracked in Server

--- Server.java
+++ Server.java
@@ -9,14 +9,18 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
-
+import java.math.BigInteger;
 
 public class Server
 {
+    static Map<String,BigInteger> countOfMessagesFromIndividualClientIDs
+            = new HashMap<String,BigInteger>();
+    
     /**
      * Constructor for objects of class Server
      */
     public Server(int tcpPortNumber)
+            throws IOException
     {
         ServerSocket serverSocket;
         Socket ioSocket;
@@ -24,44 +28,61 @@ public class Server
         BufferedReader in;
         String message;        
         
-        try {
-            serverSocket = new ServerSocket(tcpPortNumber);
-            ioSocket = serverSocket.accept();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        serverSocket = new ServerSocket(tcpPortNumber);
         
-        System.out.println("Connection accepted.");
-        try {
+        for (;;)
+        {
+            ioSocket = serverSocket.accept();
+                    
+            System.out.println("Connection accepted.");
             out = new PrintWriter(ioSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
                     ioSocket.getInputStream()));
             message = in.readLine();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            
+            System.out.println("Received message: " + message);
+            String clientId = (new Scanner(message)).next();
+            receivedMessageFrom(clientId);
+            out.println("Nice to hear from you, " + clientId + "!");
+            out.println("I have heard from you this many times: "
+                    + numberOfMessagesFromClient(clientId));
+            ioSocket.close();
         }
-        
-        System.out.println("Received message: " + message);
-        String clientId = (new Scanner(message)).next();
-        out.println("Nice to hear from you, " + clientId + "!");
-        
-        receivedMessageFrom(clientId);
         
         //maybeTellClientNotToSendAnyMoreMessages(out);
     }    
     
+    public BigInteger numberOfMessagesFromClient(String clientId)
+    {
+        BigInteger returnValue = countOfMessagesFromIndividualClientIDs.get(clientId);
+        
+        return returnValue == null ? BigInteger.ZERO : returnValue;
+    }
+    
     private void receivedMessageFrom(String clientId)
     {
-        // XXX update the message count
+        System.out.println("before: " + countOfMessagesFromIndividualClientIDs.get(clientId));
+
+        if (!countOfMessagesFromIndividualClientIDs.containsKey(clientId))
+                countOfMessagesFromIndividualClientIDs.put(clientId,
+                        BigInteger.ONE);
+        else
+                countOfMessagesFromIndividualClientIDs.put(clientId,
+                        countOfMessagesFromIndividualClientIDs.get(clientId)
+                        .add(BigInteger.ONE));
+                        
+        System.out.println("after: " + countOfMessagesFromIndividualClientIDs.get(clientId));
     }
     
     public static void runServerOnTCPPort(int tcpPortNumber)
+            throws IOException
     {
         System.out.println(tcpPortNumber);
         new Server(tcpPortNumber);
     }
     
     public static void main(String[] args)
+            throws IOException
     {
         int tcpPortNumber = new Integer(args[0]);
         runServerOnTCPPort(tcpPortNumber);
